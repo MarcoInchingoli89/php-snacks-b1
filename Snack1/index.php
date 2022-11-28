7c90e52d5ecce61f2639b9617458f1c7ac3ace38
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-28 12:51:01 +0100

    Snack1 Fixed

--- Snack1/index.php
+++ Snack1/index.php
@@ -1,7 +1,8 @@
 <?php
 //Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. Olimpia Milano - Cantù | 55-60
 
-$basketMatches =
+//Creo un array associativo in PHP
+$basketMatches2022 =
     [
         [
             "homeTeam" => "Angels",
@@ -36,4 +37,27 @@ $basketMatches =
 
     ];
 
+var_dump($basketMatches2022);
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($basketMatches2022 as $match) ?>
+        <li>
+            <?php echo $match['homeTeam'] ?>
+        </li>
+    </ul>
+</body>
+
+</html>
